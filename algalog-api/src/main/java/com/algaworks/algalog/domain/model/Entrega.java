1206ba0ca076659c2e4ac67b8e6d9f348529f1e0
Caique Azevedo Coelho Leme <caique.leme@gpnet.com.br>
2023-04-24 22:31:08 -0300

    Curso Java Mergulho Spring Rest

---
+++ algalog-api/src/main/java/com/algaworks/algalog/domain/model/Entrega.java
@@ -0,0 +1,82 @@
+package com.algaworks.algalog.domain.model;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.algaworks.algalog.domain.ValidationGroups;
+import com.algaworks.algalog.domain.exception.NegocioException;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.groups.ConvertGroup;
+import jakarta.validation.groups.Default;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Entrega {
+	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ConvertGroup(from = Default.class, to = ValidationGroups.ClienteId.class)
+	@ManyToOne
+	private Cliente cliente;
+
+	@Embedded //Mapear todos os atríbutos da outra classes dentro desta tabela
+	private Destinatario destinatario;
+
+	private BigDecimal taxa;
+	
+	@OneToMany(mappedBy = "entrega", cascade = CascadeType.ALL)
+	private List<Ocorrencia> ocorrencias = new ArrayList<>();
+	
+	@Enumerated(EnumType.STRING)
+	private StatusEntrega status;
+
+	private OffsetDateTime dataPedido;
+	
+	private OffsetDateTime dataFinalizacao;
+
+	public Ocorrencia adicionarOcorrencia(String descricao) {
+		Ocorrencia ocorrencia = new Ocorrencia();
+		ocorrencia.setDescricao(descricao);
+		ocorrencia.setDataRegistro(OffsetDateTime.now());
+		ocorrencia.setEntrega(this);
+		
+		this.getOcorrencias().add(ocorrencia);
+		
+		
+		return ocorrencia;
+	}
+
+	public void finalizar() {
+		if(!podeSerFinalizada()) {
+			throw new NegocioException("Entrega não pode ser finalizada!");
+		}
+		
+		setStatus(StatusEntrega.FINALIZADA);
+		setDataFinalizacao(OffsetDateTime.now());
+	}
+	
+	public boolean podeSerFinalizada() {
+		return StatusEntrega.PENDENTE.equals(getStatus());
+	}
+	
+}
